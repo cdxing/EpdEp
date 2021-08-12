99db12b297f176e1c3d8d1ab5550ad11c52f732c
Ding Chen <dchen@rcas6011.rcf.bnl.gov>
2021-08-12 15:58:08 -0400

    Xiaoyu's package

---
+++ Analysis.C
@@ -0,0 +1,116 @@
+#include <TSystem>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+void Analysis(Int_t nRootFilesToProcess=1, TString FileListName, TString JobIdName)
+{
+
+  if (nRootFilesToProcess==0) nRootFilesToProcess=10000000;  // i.e. all of them
+
+  gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+  loadSharedLibraries();
+
+  gSystem->Load("StPicoEvent");
+  gSystem->Load("StEpdUtil.so");  // malisa
+  gSystem->Load("FlowAnalysis.so");  // note that the flow code requires StEpdUtil library to be loaded first.
+  gSystem->Load("StRefMultCorr");
+    
+  PicoAnalyzer* pA = new PicoAnalyzer(JobIdName);
+  pA->Init();                                  // this also calls init of EP finder
+
+  TString rootFileName;
+  TChain* picoDst = new TChain("PicoDst");
+  std::ifstream ifs(FileListName,std::ifstream::in);
+  if (!ifs){std::cout << "File list " << FileListName << " does not exist!  I quit.\n ";  return;}
+  for (int iFile=0; iFile<nRootFilesToProcess; iFile++){
+    ifs >> rootFileName;
+    if (!(ifs.good())){
+      std::cout << "I ran out of files to add after " << iFile << " files\n";
+      break;
+    }
+    std::cout << " - Adding file " << rootFileName.Data() << endl;
+    picoDst->Add(rootFileName.Data());
+  }
+  pA->SetPicoDst(picoDst);
+
+  int NeventsToAnalyze = picoDst->GetEntries();
+  std::cout << "Preparing to analyze " << NeventsToAnalyze << " events...\n";
+  for (int ievent=0; ievent<picoDst->GetEntries(); ievent++){
+    if (ievent%1000==0) std::cout << "Processed " << ievent << " events - " << 100.0*(double)ievent/(double)NeventsToAnalyze << "% \n";
+    pA->Make(ievent);
+  }
+
+  pA->Finish();
+
+  cout << "Well hello there!\n";
+  cout << "Well, that was fun -- goodbye!!\n";
+
+
+  delete pA;
+  delete picoDst;
+
+
+
+  //  chain                            =  new StChain();
+  //  StPicoDstMaker*    picoMaker     =  new StPicoDstMaker(2,InputFileList,"picoDst");
+  //  MyAnalysisMaker*   AnalysisCode  =  new MyAnalysisMaker(picoMaker) ;  malisa - MUST PASS JOBNAME IN CONSTRUCTOR!!
+  //  MyAnalysisMaker*   AnalysisCode  =  new MyAnalysisMaker(picoMaker,JobIdName) ;
+
+
+  /* no.  this is all bad - malisa
+     get the unique identifier in at the very start, then open your files then book your histograms/trees/ntuples/whatever
+     that is the most robust way, rather than relying on the user to invoke methods in a particular order.  also, if another
+     code (e.g. EventPlane finder) opens TFiles on its own, every ttree/ntuple/whatever MUST be already associated with
+     an open TFile, or else there is confusion, and the TTree gets flushed into the most-recently-opened file when it grows
+     large enough that root would like to flush it.  this causes .root file corruption that is actually really hard to
+     debug!
+
+     TString Name = JobIdName ;  Name.Append(".histograms.root") ;
+     TString FemtoDSTName = JobIdName ;  FemtoDSTName.Append(".FemtoDst.root") ;
+     TString Name_Broken = JobIdName ;  Name_Broken.Append(".histograms.broken.root") ;
+     TString FemtoDSTName_Broken = JobIdName ;  FemtoDSTName_Broken.Append(".FemtoDst.broken.root") ;
+     AnalysisCode -> SetOutputFileName(Name) ;       // Name the output file for histograms
+     AnalysisCode -> SetBrokenOutputFileName(Name_Broken);
+     AnalysisCode -> SetFemtoDstFileName(FemtoDSTName) ;       // Name the output file for histograms
+     AnalysisCode -> SetBrokenFemtoDstFileName(FemtoDSTName_Broken) ;       // Name the output file for histograms
+  */
+
+  /*
+  if( chain->Init()==kStErr ){ 
+    cout<<"chain->Init();"<<endl;
+    return;
+  }
+
+  int total = picoMaker->chain()->GetEntries();
+    cout << " Total entries = " << total << endl;
+    if(nEvents>total) nEvents = total;
+
+  for (Int_t i=0; i<nEvents; i++){
+
+    if(i%1000==0)
+    cout << "Working on eventNumber " << i << endl;
+    
+    chain->Clear();
+    int iret = chain->Make(i);
+    
+    if (iret) { cout << "Bad return code!" << iret << endl; break;}
+
+    total++;
+  }
+  */
+  /*
+  cout << "****************************************** " << endl;
+  cout << "Work done... now its time to close up shop!"<< endl;
+  cout << "****************************************** " << endl;
+  chain->Finish();
+  cout << "****************************************** " << endl;
+  cout << "total number of events  " << nEvents << endl;
+  cout << "****************************************** " << endl;
+  
+  delete AnalysisCode;
+  delete picoMaker;
+  delete chain;
+  */
+}
---
+++ StRoot/FlowAnalysis/PicoAnalyzer.h
@@ -0,0 +1,204 @@
+#ifndef PicoAnalyzer__
+#define PicoAnalyzer__
+
+#include "TObject.h"
+
+class TChain;
+class TClonesArray;
+class StPicoEvent;
+class TH1D;
+class TH2D;
+class TH3D;
+class TH1F;
+class TProfile;
+class TProfile2D;
+class TProfile3D;
+class TNtuple;
+class TFile;
+class StEpdGeom;
+class StBbcGeom;
+class TRandom3;
+class StPicoEpdHit; 
+class StEpdEpFinder;
+class TLorentzVector;
+class StRefMultCorr;
+#include "StRefMultCorr/CentralityMaker.h"
+#include "TString.h"
+#include "TMath.h"
+
+#define _PsiOrderMax 1 //Maximum order of EP to worry about 
+
+class PicoAnalyzer : public TObject {
+ public:
+  PicoAnalyzer(TString FileNameBase="MikesStuff");
+  ~PicoAnalyzer();
+
+  void SetPicoDst(TChain*);
+  short Init(char const* TPCweightFileName="TPCWeightFile.root",char const*  TPCShiftFileName="TPCShiftFile.root",char const* EPDPhiWeightFile="EPDcorrection.root");
+  short Make(int iEvent);
+  short Finish();
+
+ private:
+
+  TString mFileNameBase;
+
+  // parameters relevant to my analysis
+  double mNmipQtB;  // ADC value of MIP peak on rings 6-16 (read out thru QT32Bs)
+  double mNmipQtC;  // ADC value of MIP peak on rings 1-5  (read out thru QT32Cs)
+  double mnMipThreshold;  // low-signal threshold, to cut out noise basically.
+
+  // the data objects
+  TChain*   mPicoDst;
+  TClonesArray* mEpdHits;
+  TClonesArray* mBbcHits;
+  TClonesArray* mTracks;
+  TClonesArray* mEventClonesArray;  // kind of hilarious that the StPicoEvent is stored as a one-element TClonesArray :-)
+  TClonesArray* mTraits;
+
+  // ntuples
+  TNtuple* mQ1vectorNtuple;      // Q1 vectors ring-by-ring. For offline weight optimization
+  TNtuple* mQ2vectorNtuple;      // Q2 vectors ring-by-ring. For offline weight optimization
+
+  int mRunId;                         // when this changes, refresh some information.
+  int mRunEt;                    //Run entry
+  short mRunCollisionSystem;
+
+
+
+  // internal methods
+  int FindTrackId(int Trkch,double TrkVz,double Trketa,double TrkpT );//this method only works when bin numbers are even and non-zero!!!!
+  int FindCent(int RefMult);   // utility class just giving centrality bin.  Copied directly from Isaac 1 May 2018
+  double GetBbcPmtPhi(short PmtId);
+  void ReadInSystems();    // reads a text file that idenfies the collision system for every run
+  short WhichSystem(int runId);
+  void NewRun(int runId);    // invoked when Make() detects that a new run has been loaded
+  //void FillPhiWeightHistos(StPicoEpdHit* epdHit, double weight);     // fills the histograms used for (a later job's) phi weighting
+  bool Runlist(int runId);
+  // https://drupal.star.bnl.gov/STAR/blog/lisa/optimizing-ep1-resolution-au27au-ring-dependent-weights-eta-dependent-weights
+  double v1Weight(int CentId, double eta);
+
+  // useful objects kept by PicoAnalyzer
+  StEpdGeom* mEpdGeom;
+  StBbcGeom* mBbcGeom;
+  StEpdEpFinder* mEpFinder;
+  StRefMultCorr* mRefMultCorr;
+  TRandom3* mRan;//seems like RCF only likes TRandom3
+  char mCollidingSystem[365][500];    // index1=day of year;  index2=run of day
+  
+  static const int mTPCphibin = 80;
+  double mTPCphibinwidth = TMath::Pi()/40;
+  
+  /*
+  //  static const int mNumberOfEpdSubEvents = 6;
+  //  double mEpdEtaBbounds[mNumberOfEpdSubEvents+1] = {2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0};
+  static const int mNumberOfEpdSubEvents = 3;
+  double mEpdEtaBbounds[mNumberOfEpdSubEvents+1] = {2.0, 3.0, 4.0, 5.0};
+  static const int mNumberOfTpcSubEvents = 16;
+  TProfile* mEastLongDecorProfile[mNumberOfEpdSubEvents][4];    // second index is order of event plane
+  TProfile* mWestLongDecorProfile[mNumberOfEpdSubEvents][4];
+  */
+
+  // ---------- Now, my histograms, ntuples, TFiles, etc.  All stuff particular to my analysis
+  // 1D histograms
+  TH1D* mHisto1D[40];            // miscellaneous 1D histograms
+  
+  // 2D histograms
+  TH2D* mHisto2D[40];           // miscellaneous 2D histograms
+
+  TH2D* mTPCPhiWeightOutput;    // this is used for "phi weighting" TPC tracks 
+  TH2D* mTPCPhiAveraged;        // this is just used for the normalization of the above. - not ever saved.  Only internal use
+  TH2D* mTPCPhiWeightInput;     // "phi weighting" correction factors that were calculated and saved in a PREVIOUS run
+  TH2D* mTPCPhiAveragedInput;
+
+double mpTMin;
+double mpTMax;
+double mEtaMin;//for phi-weighting, used in FindTrackId() and when MakeWeight
+double mEtaMax;////for phi-weighting
+int mNPhibin;
+double mVzMin;
+double mVzMax;
+double mEPDMax;
+double mEPDthresh;
+int mNpTbin;
+int mNVzbin;
+int mNEtabin;
+int mNumberOfTrackTypes;
+double mVtxR;
+double mDiffVzVPD;
+int mNhitsfit;
+double mNhitsfitratio;
+double mDCAcut;
+int mFourierOrder;
+int mNTPCSubEvents;
+int mNEPDSubEvents;
+double mpTbound;//For looking at low/high pT tracks
+double mPionSigma;//1/beta sigma 
+double mKaonSigma;//1/beta sigma
+double mProtonSigma;//1/beta sigma 
+double mEtaMaxv1;//for v1 analysis
+double mEtaMinv1;//for v1 analysis 
+
+//histos for checking the phi-weighted eta-phi distribution for the TPC tracks 
+  TH2D* mEtaPhiDisPhiWeighted[9];//phi-weighted eta phi distribution for 9 centralitites.
+  TH2D* mEtaPhiDisRaw[9];
+//histos for checking the pT distribution 
+  TH1F* mPtRaw[9];//9 centralities 
+  //TH1D* mpTPhiWeighted[9][10];
+  //TH1F* mPtRaw;
+  TH1D* mVz[9];//Vz dis for nine centralites
+  TH1D* mdNdeta[9];//dNdeta for nine centralities 
+
+//histos for checking the flattening of the TPC Psi
+  TH1D* mTPCPsiDisWeighted[9][3];//Entry is the centrality and "TrackType", e.g sometimes I want to look at low pT or high pT tracks.
+  TH1D* mTPCPsiDisShifted[9][3];
+//histos for checking the flattening of the Psi_EODfull
+  TH1D* mEPDFullPsiWeighted[9][_PsiOrderMax];
+  TH1D* mEPDFullPsiShifted[9][_PsiOrderMax];
+
+//TProfile for calculating the resolutions.
+  TProfile* mResolution[9][_PsiOrderMax];//Indicies are cent. 
+
+//TProfile for vn(TPC) w/ respect to Psi_EPDfull and vn(EPD) w/ respect to Psi_TPC using trucated nMIP(0.3,3.0)
+  TProfile* mTPCvn[9][_PsiOrderMax];//cent, Psi order
+  TProfile* mEPDvn[2][9][_PsiOrderMax];//EPD vn of E/W EPD, cent, Psi order 
+
+//TH3D for measuring dNdphi
+  //TH3D* mThreeD[9][2];//centralities, ew
+  //TH3D* mThreeD[9][2][_PsiOrderMax][2][16];//centralities, ew, order of harmonics, reference: 0-TPC, 1-the other side of EPD, 16 Vz bins in [-60,60] 
+  TH3D* mThreeD[2];//centralities, ew, order of harmonics, reference: 0-TPC, 1-the other side of EPD, 16 Vz bins in [-60,60] 
+  TH3D* mThreeDTile[2][24];//centralities, ew, order of harmonics, reference: 0-TPC, 1-the other side of EPD, 16 Vz bins in [-60,60] 
+  
+  //Looking at the percentage of differnt nMIPs 
+  //TH2D* mTwoD[9];//centralities, X:nMIp, Y:RingId (West-1, East-0)  
+/*
+//TProfiles for flow decorrelation analysis
+  TProfile* mRefMultvsVz;
+  TProfile* mPhiPsievent[2][9];//indicies are e/w and 9 centrality windows
+  TProfile* mTPC1TPC2[9];
+  TProfile* mPhiPsievent3[2][9];//indicies are e/w and 9 centrality windows
+*/
+  TProfile3D* mTPCCosShift;
+  TProfile3D* mTPCSinShift;
+
+  TProfile3D* mTPCCosShift_Input;
+  TProfile3D* mTPCSinShift_Input;
+
+  TProfile2D* mAveEta[9][2]; //nine centralities, EW(ring#, ivz,eta)
+
+//cp TH3D EPDPhiWeight histos from the StEpdEpFinder OUTPUT.
+  TH3D* mEPDPhiWeights[2];//e/w
+
+  // TFiles (to store histograms and data)
+  TFile* mHistoFile;
+  TFile* mTPCWeightFile;
+  TFile* mTPCShiftFile;
+  TFile* mEPDPhiWeightFile;
+  //TFile* mRecenQFile;
+
+  static const int mEPTPCMaxTerm = 6; 
+  ClassDef(PicoAnalyzer, 1)                     //  Macro for CINT compatability
+
+};
+
+
+#endif
